9200cdd7fd5665464945d490ccc7e5bb6386ddf4
Jarod Luebbert <jarod.luebbert@gmail.com>
2010-02-26 17:07:28 -0600

    Added more examples for retrieving flights

--- examples/flights.rb
+++ examples/flights.rb
@@ -4,6 +4,25 @@ flightcaster = FlightCaster.new('secret')
 
 flights = flightcaster.flights
 
+# all flights
 flights.each do |f|
-  puts f.status_code
+  puts "#{f.airline_icao_id}-#{f.number}"
+end
+
+puts "*"*50
+
+# all flights going from PDX to DFW
+flights = flightcaster.flight_path('PDX', 'DFW')
+
+flights.each do |f|
+  puts "#{f.airline_icao_id}-#{f.number}"
+end
+
+puts "*"*50
+
+# all flights with a certain airline
+flights = flightcaster.flights_by_airline('VX')
+
+flights.each do |f|
+  puts "#{f.airline_icao_id}-#{f.number}"
 end
